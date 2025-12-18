4989e287d082543992fdef1c917f067d6877dd18
David <hamiltondm@merrimack.edu>
2025-12-17 19:04:44 -0500

    small tweaks for better functionality

--- src/lexer/CharacterStream.java
+++ src/lexer/CharacterStream.java
@@ -24,42 +24,40 @@ import java.io.IOException;
 import java.io.StringReader;
 
 /**
- * 
- * A character stream object. The stream maintains the head of the stream and 
- * gives two classes of operations to mainuplate the head of the stream:
+ * A character stream object. The stream maintains the head of the stream and
+ * gives two classes of operations to manipulate the head of the stream:
  * <ol>
- *   <li> Operations that advance the stream ahead some number of characters. The
- *      operations are:
- *      <ul>
- *          <li> {@code advance}: advance the stream head to the next character. </li>
- *          <li> {@code advanceToNonBlank}: advance the stream head to the next non-blank char. </li>
- *      </ul>
+ *   <li>
+ *     Operations that advance the stream head.
+ *     <ul>
+ *       <li>{@code advance}: advance the stream head to the next character.</li>
+ *       <li>{@code advanceToNonBlank}: advance the stream head to the next non blank character.</li>
+ *     </ul>
  *   </li>
- *   <li> Operations that prevent the next advance operation from moving the 
- *      head of the stream.
- *          <ul><li>This operation is called {@code skipNextAdvance}</li></ul>
+ *   <li>
+ *     Operation that prevents the next advance operation from moving the head of the stream.
+ *     <ul>
+ *       <li>{@code skipNextAdvance}</li>
+ *     </ul>
  *   </li>
  * </ol>
- * Unless the stream position has been explicity manipulate the head of the stream stays 
- * fixed. There are two pieces of information that are relevant with respect to the 
- * stream head.
+ * The stream head exposes two relevant pieces of information:
  * <ol>
- *   <li> The character at the head of the stream {@code getCurrentChar} </li>
- *   <li> The class of the character at the head of the stream {@code getCurrentClass} </li>
+ *   <li>The character at the head of the stream: {@code getCurrentChar}</li>
+ *   <li>The class of the character at the head of the stream: {@code getCurrentClass}</li>
  * </ol>
  */
-public class CharacterStream {
-
+public class CharacterStream
+{
     private BufferedReader input; // The input to the lexer.
-    private char nextChar; // The next character read.
-    private boolean skipRead; // Whether or not to skip the next char
-                              // read.
+    private char nextChar; // The current character at the head of the stream.
+    private boolean skipRead; // Whether to skip the next char read.
     private long currentLineNumber; // The current line number being processed.
-    CharacterClass nextClass;
+    private CharacterClass nextClass; // The class of the current character.
 
     /**
      * Constructs a new character stream whose source input is a file.
-     * 
+     *
      * @param file the file to open for lexical analysis.
      * @throws FileNotFoundException if the file can not be opened.
      */
@@ -67,30 +65,38 @@ public class CharacterStream {
     {
         input = new BufferedReader(new FileReader(file));
         currentLineNumber = 1;
+        nextChar = '\0';
+        nextClass = CharacterClass.WHITE_SPACE;
+        skipRead = false;
     }
 
     /**
-     * Constructs a new character whose source is a string.
-     * 
+     * Constructs a new character stream whose source is a string.
+     *
      * @param input the input to lexically analyze.
      */
     public CharacterStream(String input)
     {
-        this.input = new BufferedReader(new StringReader(input));
+        this.input = new BufferedReader(new StringReader(input == null ? "" : input));
         currentLineNumber = 1;
+        nextChar = '\0';
+        nextClass = CharacterClass.WHITE_SPACE;
+        skipRead = false;
     }
 
     /**
      * Get the current line number being processed.
-     * 
+     *
      * @return the current line number being processed.
      */
-    public long getLineNumber() {
+    public long getLineNumber()
+    {
         return currentLineNumber;
     }
 
     /**
      * Get the value of the current character.
+     *
      * @return the character at the head of the stream.
      */
     public char getCurrentChar()
@@ -99,23 +105,22 @@ public class CharacterStream {
     }
 
     /**
-     * Get the value of the current class.
-     * @return the class of character at the head of the
-     * stream.
+     * Get the class of the current character.
+     *
+     * @return the class of character at the head of the stream.
      */
     public CharacterClass getCurrentClass()
     {
         return nextClass;
     }
 
-     /**
+    /**
      * Advances the stream one character.
      */
     public void advance()
     {
-        int c = -1;
+        int c;
 
-        // Handle the unread operation.
         if (skipRead)
         {
             skipRead = false;
@@ -128,20 +133,21 @@ public class CharacterStream {
         }
         catch (IOException ioe)
         {
-            System.err.println("Internal error (getChar()): " + ioe);
+            System.err.println("Internal error (advance): " + ioe);
             nextChar = '\0';
             nextClass = CharacterClass.END;
+            return;
         }
 
-        if (c == -1) // If there is no character to read, we've reached the end.
+        if (c == -1)
         {
             nextChar = '\0';
             nextClass = CharacterClass.END;
             return;
         }
 
-        // Set the character and determine it's class.
         nextChar = (char) c;
+
         if (Character.isLetter(nextChar))
             nextClass = CharacterClass.LETTER;
         else if (Character.isDigit(nextChar))
@@ -151,27 +157,27 @@ public class CharacterStream {
         else
             nextClass = CharacterClass.OTHER;
 
-        // Update the line counter for error checking.
         if (nextChar == '\n')
             currentLineNumber++;
     }
 
     /**
-     * Advances the stream to the next non-blank character.
+     * Advances the stream to the next non blank character.
      */
-    public void advanceToNonBlank() {
+    public void advanceToNonBlank()
+    {
         advance();
 
         while (nextClass != CharacterClass.END
-                && Character.isWhitespace(nextChar))
+                && nextClass == CharacterClass.WHITE_SPACE)
             advance();
     }
 
     /**
-     * Skips the next advance call. Multiple calls 
-     * will *not* go back further than one character.
+     * Skips the next advance call. Multiple calls will not go back further than one character.
      */
-    public void skipNextAdvance() {
+    public void skipNextAdvance()
+    {
         skipRead = true;
-    }    
+    }
 }
