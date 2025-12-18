4c4e1b20d9e394cdbb73d7f553854594fbf8287f
David <hamiltondm@merrimack.edu>
2025-12-17 19:08:46 -0500

    clean up parseexception formatting and safety

--- src/parser/ParseException.java
+++ src/parser/ParseException.java
@@ -1,9 +1,7 @@
-package parser;
-
 /*
  *   Copyright (C) 2022 -- 2025  Zachary A. Kissel
  *
- *   This program is free software: you can redistribute it and/or modify
+ *   This program is free software: you can redistribute it and or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
@@ -16,15 +14,15 @@ package parser;
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+package parser;
 
- /**
-  * A simple parsing exception class.
-  */
-public class ParseException extends Exception {
-
+/**
+ * A simple parsing exception class.
+ */
+public class ParseException extends Exception
+{
     /**
-     * The constructor for an parsing exception that 
-     * prints the line number and a statement of failure.
+     * The constructor for a parsing exception.
      */
     public ParseException()
     {
@@ -32,12 +30,12 @@ public class ParseException extends Exception {
     }
 
     /**
-     * Constructs a new parsing exception that prints the line number,
-     * statement of failure, and the reason for the failure.
+     * Constructs a new parsing exception with a reason.
+     *
      * @param reason the reason parsing failed.
      */
     public ParseException(String reason)
     {
-        super("Parsing failed. " + reason);
+        super("Parsing failed. " + (reason == null ? "" : reason));
     }
 }
